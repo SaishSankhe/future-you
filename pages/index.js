a73e78f20f2a4b6da9a4e471c5b6028c67eaa318
Saish Sankhe <saish@macbook-pro.mynetworksettings.com>
2023-10-09 23:52:59 -0400

    adds link preview, changes random note gen

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import getNote from "@/utils/getRandomNote";
-import { Architects_Daughter, Shantell_Sans } from "next/font/google";
+import { Architects_Daughter } from "next/font/google";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 const architectsDaughter = Architects_Daughter({
   subsets: ["latin"],
   weight: "400",
-});
-
-const shantellSans = Shantell_Sans({
-  subsets: ["latin"],
 });
 
 export default function Home() {
@@ -28,22 +25,40 @@ export default function Home() {
   };
 
   return (
-    <main
-      className={`flex flex-col mx-10 md:m-auto md:max-w-lg text-2xl min-h-screen ${architectsDaughter.className}`}
-    >
-      {loading && <p>Loading...</p>}
-      {!loading && (
-        <div className="m-auto leading-normal md:leading-8">
-          <p>{note.salutation},</p>
-          <p className="my-4">{note.note}</p>
-          <p>{note.closing},</p>
-          <p>Future you</p>
-        </div>
-      )}
-      <br />
-      {/* <button className="border border-black p-2" onClick={handleButtonClick}>
+    <>
+      <Head>
+        <title>From, Future You</title>
+        <meta property="og:url" content="https://fromfutureyou.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="From, Future You" />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="A note of inspiration from future you!"
+        />
+        <meta
+          property="og:image:alt"
+          content="A note of inspiration from future you!"
+        />
+        {/* <meta property="og:image" content={"preview.png"} /> */}
+      </Head>
+      <main
+        className={`flex flex-col mx-10 md:m-auto md:max-w-lg text-2xl min-h-screen ${architectsDaughter.className}`}
+      >
+        {loading && <p className="m-auto">Loading...</p>}
+        {!loading && (
+          <div className="m-auto leading-normal md:leading-8">
+            <p>{note.salutation},</p>
+            <p className="my-4">{note.note}</p>
+            <p>{note.closing},</p>
+            <p>Future you</p>
+          </div>
+        )}
+        <br />
+        {/* <button className="border border-black p-2" onClick={handleButtonClick}>
         Generate Random Name
       </button> */}
-    </main>
+      </main>
+    </>
   );
 }
--- utils/getRandomNote.js
+++ utils/getRandomNote.js
@@ -2,17 +2,17 @@ import closings from "@/data/closings";
 import notes from "@/data/notes";
 import salutations from "@/data/salutations";
 
+const TOTAL = {
+  notes: notes.length - 1,
+  closings: closings.length - 1,
+  salutations: salutations.length - 1,
+};
+
+const getRandom = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
 export default function getNote() {
-  const TOTAL = {
-    notes: notes.length - 1,
-    closings: closings.length - 1,
-    salutations: salutations.length - 1,
-  };
-
-  const getRandom = (max) => {
-    return Math.floor(Math.random() * max);
-  };
-
   return {
     salutation: salutations[getRandom(TOTAL.salutations)],
     note: notes[getRandom(TOTAL.notes)],
