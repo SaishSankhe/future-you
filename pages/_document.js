1345636f86050bc38063e03600712cdb2a99179f
Saish Sankhe <saish@macbook-pro.mynetworksettings.com>
2023-10-11 14:17:03 -0400

    fixes typo

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-bg text-conent">
+      <body className="bg-bg text-content">
         <Main />
         <NextScript />
       </body>
